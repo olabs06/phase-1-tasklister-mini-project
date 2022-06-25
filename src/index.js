aeba6d981ec3ffd24a828b39fb07d8997059e1f3
Simeon <olabs06@yahoo.com>
2022-06-25 16:42:16 +0100

    modifying index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector ('form')  
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    buildTodo(e.target.description.value)
+    form.reset()
+  
+   })
+
+   function buildTodo(todo) {
+    let list = document.createElement('li')
+    let btn = document.createElement ('button')
+    btn.addEventListener('click', handleDelete)
+    btn.addEventListener = 'x'
+    list.textContent = `${todo} `
+    list.appendChild(btn)
+    document.querySelector ('#tasks').appendChild (list) 
+   }
+
+   function handleDelete(e) {
+    e.target.parentNode.remove()
+   }
 });
